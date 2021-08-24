d2e122662995f2b695e62fa46473dffc01f59783
Francisco Catalán <francisco.catalan213@gmail.com>
2021-08-24 16:20:02 -0400

    Create login.R

---
+++ login.R
@@ -0,0 +1,45 @@
+login<-function(user,pass){
+  userVector<-c("Francisco", "Rafaella")
+  passVector<-c("123456","123456")
+  authentificationMatrix<-data.frame(userVector, passVector)
+  aux<-length(userVector)
+  check<-FALSE
+  if(user ==""){
+    check<-TRUE
+    print("Insertar usuario")
+  }
+  i=1
+  try(
+    {
+    while(check == FALSE){
+      
+      if(user == authentificationMatrix[1, i]){
+        check<-TRUE
+        checkPass<-FALSE
+        j<-1
+        try(
+          {
+          while(checkPass == FALSE){
+            if(pass == authentificationMatrix[2,j]){
+              checkPass<-TRUE
+              return(TRUE)
+              
+            }
+            j = j+1
+          }
+        }, silent = TRUE)
+        if(checkPass == FALSE){ 
+          print("Pass invalida")
+          checkPass<-FALSE}
+      }
+      
+      i=i+1
+    }
+  }, silent = T)
+    
+    
+  if(check == FALSE){ 
+    print("Usuario No Registrado")
+    check<-FALSE}
+  
+}
